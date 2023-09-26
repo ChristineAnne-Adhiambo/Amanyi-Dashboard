73189c7fbb107a862685cb2fdbfdaad3b6d4520c
Cathybiwott <biwottcathy@gmail.com>
2023-09-26 17:39:09 +0300

    created the notification message

--- aquamanyi/src/components/page.tsx
+++ aquamanyi/src/components/page.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { useState } from "react";
+// import React, {useState} from 'react';
 
 const Notification = () => {
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <div className="flex flex-col items-center pt-20">
       <div className="flex justify-end items-center w-3/4 h-29">
@@ -20,8 +23,15 @@ const Notification = () => {
 
       <div className="flex flex-col items-start mt-10 ml-10">
         <div className="text-3xl font-semibold mb-4">
-          <span className="mr-4">Messages:</span>
-        </div>
+          <span className="mr-4 mb-8">Messages:</span>
+
+          {/* <div className="flex justify-center mt-4">
+    <button className="bg-blue-500 text-white p-2 rounded" onClick={handleAlertClick}>
+      Show Alerts
+     </button>   */}
+     
+    
+        {/* </div> */}
 
         <div className="w-3/4 h-40 flex-shrink-0 border-4 border-gray-200 bg-opacity-51 bg-white-400 rounded-lg p-4 shadow-md">
           <div className="text-black font-inter text-2xl font-semibold leading-normal">
@@ -30,10 +40,29 @@ const Notification = () => {
         </div>
       </div>
     </div>
+    </div>
+    
   );
 };
 
 export default Notification;
+
+{/* // const [showAlert, setShowAlert] = useState(false);
+//   const handleAlertClick = () => { */}
+{/* //     setShowAlert(true);
+//     setTimeout(() => setShowAlert(false), 5000); // Automatically hide after 5 seconds
+//   };
+//    return (
+//     <div className="flex justify-center mt-4">
+//     <button className="bg-blue-500 text-white p-2 rounded" onClick={handleAlertClick}>
+//       Show Alerts
+//     </button>
+//     {showAlert && ( */}
+//       <div
+//  className="fixed top-1/2 left-1/2 transform -translate-x-1/2
+// -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg w-1/2">
+//         <p className="text-black">The pH levels have raised to 6 and the water is polluted.</p>
+//       </div>
 
 
 
