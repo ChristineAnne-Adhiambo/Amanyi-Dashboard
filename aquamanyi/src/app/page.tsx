cdab91bc6a5765a7968e4b1927c0583bc1cec7ad
ChristineAnne-Adhiambo <annechristineadhiambo@gmail.com>
2023-09-26 21:58:46 +0300

    seperated Signup and Signin pages

--- aquamanyi/src/app/page.tsx
+++ aquamanyi/src/app/page.tsx
@@ -1,11 +1,12 @@
 
-import SigninForm from "./signin/page"
+
 import SignupForm from "./signup/page"
 export default function Home() {
   return (
     <main>
-      <SigninForm />
-      <SignupForm />
+       <SignupForm />
+      {/* <SigninForm /> */}
+     
 
     </main>
   )
