4b069dca28296afde2168f33338e45ec354b27bc
Rebecca Nakachwa <rebeccanakachwaaa@gmail.com>
2023-10-02 22:45:53 +0300

    Added the data feature

--- aquamanyi/src/app/hooks/useGetTemp.tsx
+++ aquamanyi/src/app/hooks/useGetTemp.tsx
@@ -31,14 +31,14 @@ const useGetTemp = () => {
 
   const getTemperatureData = () => {
     return temp.map((data) => ({
-      x: new Date(data.Timestamp).toLocaleString(), // Convert to localized date and time string
+      x: new Date(data.Timestamp).toLocaleString(), 
       y: parseFloat(data.Temperature_reading),
     }));
   };
 
   const getPhData = () => {
     return temp.map((data) => ({
-      x: new Date(data.Timestamp).toLocaleString(), // Convert to localized date and time string
+      x: new Date(data.Timestamp).toLocaleString(),
       y: parseFloat(data.pH_sensor_reading),
     }));
   };
@@ -47,7 +47,7 @@ const useGetTemp = () => {
     return temp.map((data) => ({
       pH: data.pH_sensor_reading,
       temperature: data.Temperature_reading,
-      time: new Date(data.Timestamp).toLocaleString(), // Convert to localized date and time string
+      time: new Date(data.Timestamp).toLocaleString(), 
     }));
   };
 
@@ -61,78 +61,3 @@ const useGetTemp = () => {
 };
 
 export default useGetTemp;
-
-
-
-
-
-
-
-
-
-
-
-
-// import { getTemp } from "../Utilities/utils";
-// import { useEffect, useState } from "react";
-
-// interface TempData {
-//   id: number;
-//   pH_sensor_reading: string;
-//   Temperature_reading: string;
-//   Timestamp: string;
-//   sensor: number;
-// }
-
-// const useGetTemp = () => {
-//   const [temp, setTemp] = useState<TempData[]>([]);
-//   const [loading, setLoading] = useState<boolean>(true);
-//   const [error, setError] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const tempData = await getTemp();
-//         setTemp(tempData);
-//         setLoading(false);
-//       } catch (error) {
-//         setError("Error fetching temperature data.");
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const getTemperatureData = () => {
-//     return temp.map((data) => ({
-//       x: new Date(data.Timestamp),
-//       y: parseFloat(data.Temperature_reading),
-//     }));
-//   };
-
-//   const getPhData = () => {
-//     return temp.map((data) => ({
-//       x: new Date(data.Timestamp),
-//       y: parseFloat(data.pH_sensor_reading),
-//     }));
-//   };
-
-//   const getTableData = () => {
-//     return temp.map((data) => ({
-//       pH: data.pH_sensor_reading,
-//       temperature: data.Temperature_reading,
-//       time: data.Timestamp,
-//     }));
-//   };
-
-//   return {
-//     loading,
-//     error,
-//     temperatureData: getTemperatureData(),
-//     phData: getPhData(),
-//     tableData: getTableData(),
-//   };
-// };
-
-// export default useGetTemp;
